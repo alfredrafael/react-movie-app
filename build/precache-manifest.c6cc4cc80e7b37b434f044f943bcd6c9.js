a521ac7bf1eb24650225d147e4302ac03bb20691
alfredrafael <alfredrafael@gmail.com>
2021-11-23 01:49:40 -0500

    ran 'yarn run build'

---
+++ build/precache-manifest.c6cc4cc80e7b37b434f044f943bcd6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becdbac5ce4185b9bea32e17a7bda4e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1a7df07169e966f75d",
+    "url": "/static/js/2.6db30916.chunk.js"
+  },
+  {
+    "revision": "3f07550778b05485deae",
+    "url": "/static/js/main.fdec56b1.chunk.js"
+  },
+  {
+    "revision": "c4ba3172e7ccc066544c",
+    "url": "/static/js/runtime~main.4b1460cd.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
